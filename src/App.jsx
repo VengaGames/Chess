b7aa1f976f408883962cc0bcc6e1f2fed0862830
Benoitmnt <123063136+Benoitmnt@users.noreply.github.com>
2023-05-27 21:44:18 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Init from "./scenes/init";
+import Game from "./scenes/game";
+
+function App() {
+  return (
+    <div className="App">
+      <Init />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
